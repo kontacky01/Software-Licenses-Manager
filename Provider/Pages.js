8579573edcbd32e907d7b4a053324952dea4606a
kontacky01 <uwasen01@sunyjcc.edu>
2023-11-03 02:27:28 -0400

    Update 1) i just debugged login. so now we check for credentials if email matches what we have in the table provider is taken to homepage pther is prompted to try again 2) i debugged Signup button on login page. you know how when you are on the login page and you clicked on signup button instead of going to the signup page it was giving file unfound error? i just changed the name of the file from SignUp.html to CreateAccount.html and now it works YAYYYY 3) for some reason MyAccount tab was not showing but it was a tag that wasn't closed the <div></di> so i fixed that 4) i added functionality to the logout button so now it's clickable to take the provider back to login page 5) i added the function to issue a license -- checkout javascript file under issueLicense..i still need to debug

--- Provider/Pages.js
+++ Provider/Pages.js
@@ -25,8 +25,8 @@ function populateClientForm(row){
     var fname = clientsTable.rows[row + 1].cells[1].innerText;
     var addr = clientsTable.rows[row + 1].cells[2].innerText;
     var softw = clientsTable.rows[row + 1].cells[3].innerText;
-    var issuedate = clientsTable.rows[row + 1].cells[4].innerText;
-    var expirydate = clientsTable.rows[row + 1].cells[5].innerText;
+    var issuedate = clientsTable.rows[row + 1].cells[5].innerText;
+    var expirydate = clientsTable.rows[row + 1].cells[6].innerText;
 
     form1.fullname.value = fname;
     form1.address.value = addr;
@@ -62,8 +62,8 @@ function renewLicense() {
     var expiryDate = future.toDateString().substring(4);
   
     // Update the "issuedate" and "future" in the table
-    selectedRow.cells[4].textContent = currentDate;
-    selectedRow.cells[5].textContent = expiryDate;
+    selectedRow.cells[5].textContent = currentDate;
+    selectedRow.cells[6].textContent = expiryDate;
   
     // Hide the "Renew License" button
     document.getElementById('renewlicense').hidden = true;
@@ -83,7 +83,7 @@ function cancelLicense() {
     var currentDate = today.toDateString().substring(4);
   
     // Update the "expirydate" in the table
-    selectedRow.cells[5].textContent = currentDate;
+    selectedRow.cells[6].textContent = currentDate;
   
     // Hide the "Cancel License" button
     document.getElementById('cancellicense').hidden = true;
@@ -99,7 +99,7 @@ function blockClient() {
     var table = document.getElementById("clientsTable");
     var selectedRow = table.rows[currentSelectedRow + 1];
     // update status of client in the table
-    selectedRow.cells[7].textContent = "Blocked";
+    selectedRow.cells[8].textContent = "Blocked";
 
     document.getElementById("blockclient").hidden = true;
     
@@ -114,7 +114,7 @@ function blockClient() {
     var selectedRow = table.rows[currentSelectedRow + 1];
 
     // update status of client in the table
-    selectedRow.cells[7].textContent = "Active";
+    selectedRow.cells[8].textContent = "Active";
 
     document.getElementById("unblockclient").hidden = true;
 
@@ -143,37 +143,95 @@ function myFunction() {
     }
 }
 /**
+ * this function egenerates a random 8 digit serial number
+ */
+function generateSN() {
+    const min = 10000000;
+    const max = 99999999;
+  
+    // Generate a random number between min and max (inclusive)
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  
+    return randomNumber;
+}
+/**
+ * this function issues licenses which includes generating a random serial number for each new license
+ */
+function issuelicense() {
+    var f = document.getElementById("issueLicenseForm");
+    var table = document.getElementById('clientsTable');
+
+    // Get the current date and calculate the new expiry date (one year from the current date)
+    var today = new Date();
+    var future = new Date(today);
+    future.setFullYear(today.getFullYear() + 1);
+
+    // Format the current and new expiry dates as strings in the format of "Mon DD YYYY"
+    var currentDate = today.toDateString().substring(4);
+    var expiryDate = future.toDateString().substring(4);
+
+    // Create a new row and cells for the record
+    var newRow = table.insertRow(table.rows.length);
+    var cellClientId = newRow.insertCell(0);
+    var cellName = newRow.insertCell(1);
+    var cellAddress = newRow.insertCell(2);
+    var cellSoftware = newRow.insertCell(3);
+    var cellSerialNumber = newRow.insertCell(4);
+    var cellIssuedOn = newRow.insertCell(5);
+    var cellExpiresOn = newRow.insertCell(6);
+    var cellEmail = newRow.insertCell(7);
+    var cellStatus = newRow.insertCell(8);
+
+    // Populate the cells with data
+    cellClientId.innerHTML = f.getElementById('id').value;
+    cellName.innerHTML = f.getElementById('fn').value;
+    cellAddress.innerHTML = f.getElementById('adr').value;
+    cellSoftware.innerHTML = f.getElementById('sft').value;
+    cellSerialNumber = generateSN();
+    cellIssuedOn.innerHTML = currentDate;
+    cellExpiresOn.innerHTML = expiryDate;
+    cellEmail.innerHTML = '<a href="mailto:' + f.getElementById('em').value + '"><button type="button" id="email">Send email</button></a>';
+    cellStatus.innerHTML = 'Active';// Default status
+    
+}
+/**
  * this function  gets details of the provider who is currently logged in
  */
 function populateProviderForm() {
+
     var enteredEmail = document.getElementById("logInEmail").value;
+    var form2 = document.getElementById("providerDetailsForm");
     var providersTable = document.getElementById("providersTable");
     var rows = providersTable.getElementsByTagName("tr");
     for (var i = 1; i < rows.length; i++) {
         // Gets each email of the provider from the table
         var providerEmail = rows[i].getElementsByTagName("td")[3].textContent; // since email is in the 4th column
-        // Checks if the entered email matches each provider's email
-        if (enteredEmail === providerEmail) {
-            var companyname = rows[i].getElementsByTagName("td")[0].textContent;
-            var address = rows[i].getElementsByTagName("td")[1].textContent;
-            var phonenumber = rows[i].getElementsByTagName("td")[2].textContent;
 
-            // Populate the fields in the provider details form
-            document.getElementById("companyname").value = companyname;
-            document.getElementById("provideraddress").value = address;
-            document.getElementById("providerphonenumber").value = phonenumber;
-            document.getElementById("provideremail").value = enteredEmail;
+        // Checks if the entered email matches each provider's email
+        if (i === rows.length-1 && enteredEmail !== providerEmail) {
+            confirm("Incorrect Email or Password. Try Again");
+        }
+        if (enteredEmail === providerEmail) {
+            //redirect to main page of the website once the user has been authenticated
+            window.location.href = "MainPage.html";
 
             //enables the fields in the provider details form to make them editable
             document.getElementById("companyname").disabled = false;
             document.getElementById("provideraddress").disabled = false;
             document.getElementById("providerphonenumber").disabled = false;
             document.getElementById("provideremail").disabled = false;
-            
-            //redirect to main page of the website once the user has been authenticated
-            //window.location.href = "MainPage.html";
-            //window.location.assign("file:///C:/Users/konta/OneDrive/Desktop/SOEN%20287/Software-Licenses-Manager/Provider/MainPage.html");
-            
+
+            var companyname = rows[i].getElementsByTagName("td")[0].textContent;
+            console.log("companyname is: "+companyname);
+            var address = rows[i].getElementsByTagName("td")[1].textContent;
+            var phonenumber = rows[i].getElementsByTagName("td")[2].textContent;
+
+            // Populate the fields in the provider details form on MyAccount tab
+            form2.companyname.value = companyname;
+            form2.provideraddress.value = address;
+            form2.providerphonenumber.value = phonenumber;
+            form2.provideremail.value = enteredEmail;
+
             // Break the loop since a match has been found
             break;
         }
